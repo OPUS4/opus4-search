cbc2f92f4ad3a1381eab369175bbe75e12c3b4d2
j3nsch <j3nsch@gmail.com>
2019-08-22 11:41:25 +0200

    OPUSVIER-3871 Fixed APPLICATION_PATH for creating database.

--- test/TestAsset/createdb.php
+++ test/TestAsset/createdb.php
@@ -32,7 +32,7 @@
 
 // Define path to application directory
 defined('APPLICATION_PATH')
-|| define('APPLICATION_PATH', realpath(dirname(dirname(__FILE__))));
+|| define('APPLICATION_PATH', realpath(dirname(dirname(dirname(__FILE__)))));
 
 // Define application environment
 defined('APPLICATION_ENV')
