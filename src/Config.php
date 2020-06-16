1e0a03f62f97ed0a8194ec42c9ecb251484552e2
j3nsch <j3nsch@gmail.com>
2020-06-16 18:34:14 +0200

    OPUSVIER-4260 Fixed bug.

--- src/Config.php
+++ src/Config.php
@@ -362,7 +362,7 @@ class Config
         if (! in_array('doctype', $set)) {
             $set[] = 'doctype';
         }
-        if (! in_array('year', $set) && ! in_array('year_inverted')) {
+        if (! in_array('year', $set) && ! in_array('year_inverted', $set)) {
             $set[] = 'year';
         }
 
