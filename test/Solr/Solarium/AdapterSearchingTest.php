e635d6f5727a912760ae7e9d3a69930217e4b6e2
Matthias Steffens <mat@extracts.de>
2023-12-01 21:55:41 +0100

    #37 More (and more granular) tests that test weighted search behavior

--- test/Solr/Solarium/AdapterSearchingTest.php
+++ test/Solr/Solarium/AdapterSearchingTest.php
@@ -307,21 +307,24 @@ class AdapterSearchingTest extends DocumentBasedTestCase
         $this->assertEquals(1, $result->getAllMatchesCount());
     }
 
-    public function testStandardAndWeightedSearch()
+    /**
+     * Test that a standard `AND` search (which uses Solr's standard query parser)
+     * finds all documents that contain both query terms in the same field.
+     */
+    public function testStandardAndSearch()
     {
-        $docA = $this->createDocument('weightedTestDocA');
-        $docB = $this->createDocument('weightedTestDocB');
-        $docC = $this->createDocument('weightedTestDocC');
-        $docD = $this->createDocument('weightedTestDocD');
-        $docE = $this->createDocument('weightedTestDocE');
+        // TODO when the `text` field gets removed from the Solr index, weightedTestDocE should not get found anymore
+        $docA = $this->createDocument('weightedTestDocA'); // full query string only occurs in abstract
+        $docB = $this->createDocument('weightedTestDocB'); // full query string only occurs in title
+        $docC = $this->createDocument('weightedTestDocC'); // has only one query term (in abstract)
+        $docD = $this->createDocument('weightedTestDocD'); // has only one query term (in abstract)
+        $docE = $this->createDocument('weightedTestDocE'); // title & abstract contain one query term each
         $this->indexDocuments([$docA, $docB, $docC, $docD, $docE]);
 
         $search = Service::selectSearchingService(null, 'solr');
         $query  = $this->queryWithSearchString($search, 'test document');
 
-        // TODO better explanation (expectation, what gets tested
-        // 1. standard search (AND)
-        $query->setWeightedSearch(false);
+        $query->setWeightedSearch(false); // use Solr's standard query parser
         $query->setUnion(false); // use AND as default query operator
 
         $result      = $search->customSearch($query);
@@ -329,15 +332,30 @@ class AdapterSearchingTest extends DocumentBasedTestCase
 
         $this->assertEquals(3, count($matchingIds));
 
+        // expect only documents that contain both query terms in the same field
         $this->assertTrue(in_array($docA->getId(), $matchingIds));
         $this->assertTrue(in_array($docB->getId(), $matchingIds));
         $this->assertTrue(in_array($docE->getId(), $matchingIds));
+    }
 
-// TODO split into separate tests
-        // 2. weighted search (AND)
-        $query->setWeightedSearch(true);
+    /**
+     * Test that a weighted `AND` search finds all documents that contain both
+     * query terms in the same field.
+     */
+    public function testWeightedAndSearchWithoutBoosts()
+    {
+        $docA = $this->createDocument('weightedTestDocA'); // full query string only occurs in abstract
+        $docB = $this->createDocument('weightedTestDocB'); // full query string only occurs in title
+        $docC = $this->createDocument('weightedTestDocC'); // has only one query term (in abstract)
+        $docD = $this->createDocument('weightedTestDocD'); // has only one query term (in abstract)
+        $docE = $this->createDocument('weightedTestDocE'); // title & abstract contain one query term each
+        $this->indexDocuments([$docA, $docB, $docC, $docD, $docE]);
+
+        $search = Service::selectSearchingService(null, 'solr');
+        $query  = $this->queryWithSearchString($search, 'test document');
+
+        $query->setWeightedSearch(true); // use Solr's eDisMax query parser
         $query->setWeightedFields(['abstract' => 1.0, 'title' => 1.0]); // assigns boost factors to fields
-        $query->setWeightMultiplier(5); // multiplier to further increase boost factors when matching phrases
         $query->setUnion(false); // use AND as default query operator
 
         $result      = $search->customSearch($query);
@@ -345,10 +363,57 @@ class AdapterSearchingTest extends DocumentBasedTestCase
 
         $this->assertEquals(2, count($matchingIds));
 
+        // expect only documents that contain both query terms in the same field
         $this->assertTrue(in_array($docA->getId(), $matchingIds));
         $this->assertTrue(in_array($docB->getId(), $matchingIds));
+    }
 
-        // 3. weighted search (OR), expect much greater scores for the two documents matching the full query phrase
+    /**
+     * Test that a weighted `OR` search finds all documents that contain at least
+     * one query term in one of their fields.
+     */
+    public function testWeightedOrSearchWithoutBoosts()
+    {
+        $docA = $this->createDocument('weightedTestDocA'); // full query string only occurs in abstract
+        $docB = $this->createDocument('weightedTestDocB'); // full query string only occurs in title
+        $docC = $this->createDocument('weightedTestDocC'); // has only one query term (in abstract)
+        $docD = $this->createDocument('weightedTestDocD'); // has only one query term (in abstract)
+        $docE = $this->createDocument('weightedTestDocE'); // title & abstract contain one query term each
+        $this->indexDocuments([$docA, $docB, $docC, $docD, $docE]);
+
+        $search = Service::selectSearchingService(null, 'solr');
+        $query  = $this->queryWithSearchString($search, 'test document');
+
+        $query->setWeightedSearch(true);
+        $query->setWeightedFields(['abstract' => 1.0, 'title' => 1.0]);
+        $query->setUnion(true); // use OR as default query operator
+
+        $result  = $search->customSearch($query);
+        $matches = $result->getReturnedMatches();
+
+        // expect all of the above documents to get found
+        $this->assertEquals(5, count($matches));
+    }
+
+    /**
+     * Test that a weighted `OR` search with boosted phrase matching results in increased
+     * importance given to search results containing an exact occurrence of the search string.
+     */
+    public function testWeightedOrSearchWithBoostedPhraseMatching()
+    {
+        $docA = $this->createDocument('weightedTestDocA'); // full query string only occurs in abstract
+        $docB = $this->createDocument('weightedTestDocB'); // full query string only occurs in title
+        $docC = $this->createDocument('weightedTestDocC'); // has only one query term (in abstract)
+        $docD = $this->createDocument('weightedTestDocD'); // has only one query term (in abstract)
+        $docE = $this->createDocument('weightedTestDocE'); // title & abstract contain one query term each
+        $this->indexDocuments([$docA, $docB, $docC, $docD, $docE]);
+
+        $search = Service::selectSearchingService(null, 'solr');
+        $query  = $this->queryWithSearchString($search, 'test document');
+
+        $query->setWeightedSearch(true);
+        $query->setWeightedFields(['abstract' => 1.0, 'title' => 1.0]);
+        $query->setWeightMultiplier(5); // multiplier to further increase boost factors when matching phrases
         $query->setUnion(true); // use OR as default query operator
 
         $result  = $search->customSearch($query);
@@ -356,7 +421,12 @@ class AdapterSearchingTest extends DocumentBasedTestCase
 
         $this->assertEquals(5, count($matches));
 
-// TODO also compare doc IDs
+        // expect the two documents matching the exact occurrence of the search string to sort first
+        $highestScoringIds = [$matches[0]->getDocument()->getId(), $matches[1]->getDocument()->getId()];
+        $this->assertTrue(in_array($docA->getId(), $highestScoringIds));
+        $this->assertTrue(in_array($docB->getId(), $highestScoringIds));
+
+        // expect much greater scores for the two documents matching the exact occurrence of the search string
         $this->assertTrue($matches[0]->getScore() > 1.0);
         $this->assertTrue($matches[1]->getScore() > 1.0);
 
@@ -365,52 +435,84 @@ class AdapterSearchingTest extends DocumentBasedTestCase
         $this->assertTrue($matches[4]->getScore() < 1.0);
     }
 
-    public function testWeightedSearch()
+    /**
+     * Test that a weighted `AND` search with a field's boost factor set to 0 will
+     * cause a document with a match just in that field to get a score of 0.
+     */
+    public function testWeightedAndSearchWithZeroedBoost()
     {
-        $docA = $this->createDocument('weightedTestDocA');
-        $docB = $this->createDocument('weightedTestDocB');
-        $this->indexDocuments([$docA, $docB]);
+        $docA = $this->createDocument('weightedTestDocA'); // full query string only occurs in abstract
+        $docB = $this->createDocument('weightedTestDocB'); // full query string only occurs in title
+        $docE = $this->createDocument('weightedTestDocE'); // title & abstract contain one query term each
+        $this->indexDocuments([$docA, $docB, $docE]);
 
         $search = Service::selectSearchingService(null, 'solr');
         $query  = $this->queryWithSearchString($search, 'test document');
 
-        // 1. with different boost factors assigned to fields, expect clearly different scores & appropriate sort order
-        $this->adjustConfiguration([
-            'search' => [
-                'weightedSearch' => true, // use the Solr eDisMax query parser
-                'simple'         => [
-                    'abstract' => 0.5,
-                    'title'    => 10,
-                ],
-            ],
-        ]);
+        $query->setWeightedSearch(true);
+        $query->setWeightedFields(['abstract' => 0, 'title' => 1.0]);
+        $query->setUnion(false); // use AND as default query operator
 
         $result  = $search->customSearch($query);
         $matches = $result->getReturnedMatches();
 
+        // expect only docA & docB to get found (which both contain the full query string in one of their fields)
         $this->assertEquals(2, count($matches));
 
-        $this->assertTrue(abs($matches[0]->getScore() - $matches[1]->getScore()) > 1.0);
-
+        // expect docB (contains full query string in title) to sort first and with a score greater than 0
         $this->assertEquals($docB->getId(), $matches[0]->getDocument()->getId());
+        $this->assertTrue($matches[0]->getScore() > 0.0);
 
-        // 2. with swapped boost factors, expect a swapped sort order
-        $query->setWeightedFields(['abstract' => 10.0, 'title' => 0.5]);
-
-        $result  = $search->customSearch($query);
-        $matches = $result->getReturnedMatches();
-
-        $this->assertEquals(2, count($matches));
-
-        $this->assertTrue(abs($matches[0]->getScore() - $matches[1]->getScore()) > 1.0);
-
-        $this->assertEquals($docA->getId(), $matches[0]->getDocument()->getId());
+        // expect docA (contains full query string in abstract) to sort last and with a score of 0
+        $this->assertEquals($docA->getId(), $matches[1]->getDocument()->getId());
+        $this->assertTrue($matches[1]->getScore() === 0.0);
     }
 
+    /**
+     * Test that a weighted `OR` search with a field's boost factor set to 0 will
+     * cause a document with a match just in that field to get a score of 0.
+     */
+    public function testWeightedOrSearchWithZeroedBoost()
+    {
+        $docB = $this->createDocument('weightedTestDocB'); // full query string only occurs in title
+        $docD = $this->createDocument('weightedTestDocD'); // has only one query term (in abstract)
+        $docE = $this->createDocument('weightedTestDocE'); // title & abstract contain one query term each
+        $this->indexDocuments([$docB, $docD, $docE]);
+
+        $search = Service::selectSearchingService(null, 'solr');
+        $query  = $this->queryWithSearchString($search, 'test document');
+
+        $query->setWeightedSearch(true);
+        $query->setWeightedFields(['abstract' => 0, 'title' => 1.0]);
+        $query->setUnion(true); // use OR as default query operator
+
+        $result  = $search->customSearch($query);
+        $matches = $result->getReturnedMatches();
+
+        // expect all documents to get found since all of them contain at least one query term in one of their fields
+        $this->assertEquals(3, count($matches));
+
+        // expect docB (contains full query string in title) to sort first and with a score greater than 0
+        $this->assertEquals($docB->getId(), $matches[0]->getDocument()->getId());
+        $this->assertTrue($matches[0]->getScore() > 0.0);
+
+        // expect docE (contains part of query string in title) to sort in the middle and with a score greater than 0
+        $this->assertEquals($docE->getId(), $matches[1]->getDocument()->getId());
+        $this->assertTrue($matches[1]->getScore() > 0.0);
+
+        // expect docD (contains part of query string in abstract) to sort last and with a score of 0
+        $this->assertEquals($docD->getId(), $matches[2]->getDocument()->getId());
+        $this->assertTrue($matches[2]->getScore() === 0.0);
+    }
+
+    /**
+     * Test that a weighted search with equal weights (i.e. no fields being boosted) will result in
+     * similar scores for two documents that both contain the full query string in one of their fields.
+     */
     public function testWeightedSearchWithEqualWeights()
     {
-        $docA = $this->createDocument('weightedTestDocA');
-        $docB = $this->createDocument('weightedTestDocB');
+        $docA = $this->createDocument('weightedTestDocA'); // full query string only occurs in abstract
+        $docB = $this->createDocument('weightedTestDocB'); // full query string only occurs in title
         $this->indexDocuments([$docA, $docB]);
 
         $search = Service::selectSearchingService(null, 'solr');
@@ -428,7 +530,7 @@ class AdapterSearchingTest extends DocumentBasedTestCase
 
         $this->assertTrue(abs($matches[0]->getScore() - $matches[1]->getScore()) < 1.0);
 
-        // 2. with equal boost factors, expect roughly equal scores
+        // 2. with equal boost factors, also expect roughly equal scores
         $query->setWeightedFields(['abstract' => 1.0, 'title' => 1.0]);
 
         $result  = $search->customSearch($query);
@@ -437,6 +539,69 @@ class AdapterSearchingTest extends DocumentBasedTestCase
         $this->assertEquals(2, count($matches));
 
         $this->assertTrue(abs($matches[0]->getScore() - $matches[1]->getScore()) < 1.0);
+    }
+
+    /**
+     * Test that a weighted search with different boost factors assigned to fields will influence
+     * result scores accordingly & cause a document with a match in a boosted field to sort first.
+     */
+    public function testWeightedSearchWithBoostedFields()
+    {
+        $docA = $this->createDocument('weightedTestDocA'); // full query string only occurs in abstract
+        $docB = $this->createDocument('weightedTestDocB'); // full query string only occurs in title
+        $this->indexDocuments([$docA, $docB]);
+
+        $search = Service::selectSearchingService(null, 'solr');
+        $query  = $this->queryWithSearchString($search, 'test document');
+
+        $this->adjustConfiguration([
+            'search' => [
+                'weightedSearch' => true, // use the Solr eDisMax query parser
+                'simple'         => [
+                    'abstract' => 0.5, // decrease importance of abstract field
+                    'title'    => 10, // increase importance of title field
+                ],
+            ],
+        ]);
+
+        $result  = $search->customSearch($query);
+        $matches = $result->getReturnedMatches();
+
+        $this->assertEquals(2, count($matches));
+
+        // expect clearly different scores between a document with a match in the boosted title field and one without
+        $this->assertTrue(abs($matches[0]->getScore() - $matches[1]->getScore()) > 1.0);
+
+        // expect the document containing the query string in the boosted title field to sort first
+        $this->assertEquals($docB->getId(), $matches[0]->getDocument()->getId());
+    }
+
+    /**
+     * Test that a weighted search with (compared to the previous test) swapped boost factors
+     * will also cause the sort order of search results to get swapped.
+     */
+    public function testWeightedSearchWithBoostedFieldsSwapped()
+    {
+        $docA = $this->createDocument('weightedTestDocA'); // full query string only occurs in abstract
+        $docB = $this->createDocument('weightedTestDocB'); // full query string only occurs in title
+        $this->indexDocuments([$docA, $docB]);
+
+        $search = Service::selectSearchingService(null, 'solr');
+        $query  = $this->queryWithSearchString($search, 'test document');
+
+        $query->setWeightedSearch(true);
+        $query->setWeightedFields(['abstract' => 10.0, 'title' => 0.5]); // increase importance of abstract field
+
+        $result  = $search->customSearch($query);
+        $matches = $result->getReturnedMatches();
+
+        $this->assertEquals(2, count($matches));
+
+        // expect clearly different scores between a document with a match in the boosted abstract field and one without
+        $this->assertTrue(abs($matches[0]->getScore() - $matches[1]->getScore()) > 1.0);
+
+        // expect the document containing the query string in the boosted abstract field to sort first
+        $this->assertEquals($docA->getId(), $matches[0]->getDocument()->getId());
     }
 
     /**
